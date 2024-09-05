9b7b421eca5f353ec51417c49d89ebfa138ee932
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-09-06 00:12:24 +0700

    feat(SystemResult): add basic monad operators

--- System/core/common/core/operation/SystemResult.kt
+++ System/core/common/core/operation/SystemResult.kt
@@ -20,8 +20,20 @@ sealed class SystemResult<out T> {
         }
 }
 
+inline fun <T,R> SystemResult<T>.map(block: (T) -> R): SystemResult<R> =
+    when(this) {
+        is Success -> Success(block.invoke(this.data))
+        is Error -> this
+    }
+
 inline fun <T> SystemResult<T>.getOrElse(block: (SystemException) -> T): T =
     when (this) {
         is Success<T> -> this.data
         is Error -> block.invoke(this.e)
+    }
+
+inline fun <T, R> SystemResult<T>.flatMap(block: (T) -> SystemResult<R>): SystemResult<R> =
+    when(this) {
+        is Success -> block.invoke(this.data)
+        is Error -> this
     }
