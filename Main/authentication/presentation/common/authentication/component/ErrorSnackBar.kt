b8d724fa997c3ff838029eac3f85113fa977d4d8
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-09-06 17:41:34 +0700

    feat(ErrorSnackBar): update error snackbar

--- Main/authentication/presentation/common/authentication/component/ErrorSnackBar.kt
+++ Main/authentication/presentation/common/authentication/component/ErrorSnackBar.kt
@@ -7,16 +7,19 @@ import androidx.compose.material3.Snackbar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import core.ui.SingularityScope
 import core.ui.designsystem.DesignToken
 import core.ui.designsystem.component.Expand
 import core.ui.designsystem.component.SIconButton
 import core.ui.designsystem.component.SSmallIcon
 import core.ui.designsystem.component.STextLabel
+import core.ui.designsystem.`error-container`
 import core.ui.designsystem.`medium-padding`
+import core.ui.designsystem.`on-error-container`
 import org.jetbrains.compose.resources.painterResource
 import system.designsystem.resources.Res
-import system.designsystem.resources.logo_of_singularity_indonesia_circle
+import system.designsystem.resources.ic_close
 
 context(SingularityScope)
 @Composable
@@ -27,7 +30,11 @@ fun ErrorSnackBar(
 ) {
     val attr = DesignToken.current
     AnimatedVisibility(visible = visible) {
-        Snackbar(modifier = Modifier.padding(horizontal = attr.`medium-padding`)) {
+        Snackbar(
+            modifier = Modifier.padding(horizontal = attr.`medium-padding`),
+            containerColor = attr.`error-container`,
+            contentColor = attr.`on-error-container`,
+        ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
             ) {
@@ -35,8 +42,9 @@ fun ErrorSnackBar(
                 Expand()
                 SIconButton(onClick = onClose) {
                     SSmallIcon(
-                        painter = painterResource(Res.drawable.logo_of_singularity_indonesia_circle),
+                        painter = painterResource(Res.drawable.ic_close),
                         contentDescription = null,
+                        colorFilter = ColorFilter.tint(attr.`on-error-container`),
                     )
                 }
             }
