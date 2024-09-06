d12b1ad8fdc0e3e5809335c46204577f284818c6
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-09-06 16:42:33 +0700

    feat(BottomNavigation): add proper icon

--- Main/dashboard/presentation/common/dashboard/component/BottomNavigation.kt
+++ Main/dashboard/presentation/common/dashboard/component/BottomNavigation.kt
@@ -14,12 +14,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import system.designsystem.resources.Res
-import system.designsystem.resources.logo_of_singularity_indonesia_circle
+import system.designsystem.resources.ic_favorite
+import system.designsystem.resources.ic_favorite_fill
+import system.designsystem.resources.ic_looks
+import system.designsystem.resources.ic_looks_fill
+import system.designsystem.resources.ic_person
+import system.designsystem.resources.ic_person_fill
+import system.designsystem.resources.ic_search
+import system.designsystem.resources.ic_search_fill
 
 @Composable
 fun BottomNavigation() {
     var selectedItem by remember { mutableIntStateOf(0) }
-    val items = listOf("Spectrum", "Memories", "Search", "Profile")
+    val items =
+        listOf(
+            "Spectrum" to Res.drawable.ic_looks to Res.drawable.ic_looks_fill,
+            "Memories" to Res.drawable.ic_favorite to Res.drawable.ic_favorite_fill,
+            "Search" to Res.drawable.ic_search to Res.drawable.ic_search_fill,
+            "Profile" to Res.drawable.ic_person to Res.drawable.ic_person_fill,
+        )
 
     NavigationBar {
         items.forEachIndexed { index, item ->
@@ -27,11 +40,11 @@ fun BottomNavigation() {
                 icon = {
                     Image(
                         modifier = Modifier.size(24.dp),
-                        painter = painterResource(Res.drawable.logo_of_singularity_indonesia_circle),
-                        contentDescription = item,
+                        painter = painterResource(if (index == selectedItem) item.second else item.first.second),
+                        contentDescription = item.first.first,
                     )
                 },
-                label = { Text(item) },
+                label = { Text(item.first.first) },
                 selected = selectedItem == index,
                 onClick = { selectedItem = index },
             )
