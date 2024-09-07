625d3500fb846a06db71cf6c6b0234de049fa368
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-09-07 15:25:50 +0700

    fix(MainActivity): status bar color

--- Main/composeApp/android/main/MainActivity.kt
+++ Main/composeApp/android/main/MainActivity.kt
@@ -26,27 +26,33 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun SetStatusBarColor() {
-
-        @Suppress("DEPRECATION")
-        val darkDecorView = window.decorView.systemUiVisibility
-
-        @Suppress("DEPRECATION")
-        val lightDecorView = window.decorView.systemUiVisibility or
-                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-
         val isDarkTheme = isSystemInDarkTheme()
 
         with(window) {
             statusBarColor = Color.TRANSPARENT
 
-            decorView.systemUiVisibility = if (isDarkTheme)
-                darkDecorView
-            else
-                lightDecorView
+            @Suppress("DEPRECATION")
+            val darkDecorView =
+                window.decorView.systemUiVisibility or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR xor
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+            @Suppress("DEPRECATION")
+            val lightDecorView =
+                window.decorView.systemUiVisibility xor
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+            decorView.systemUiVisibility =
+                if (isDarkTheme) {
+                    darkDecorView
+                } else {
+                    lightDecorView
+                }
 
             WindowCompat.setDecorFitsSystemWindows(
                 window,
-                false
+                false,
             )
         }
     }
