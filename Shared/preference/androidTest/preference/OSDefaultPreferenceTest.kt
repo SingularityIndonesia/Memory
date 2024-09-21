f9747ecba2e1345e6b35e275fea4e5071ca12ca3
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-09-21 18:41:15 +0700

    feat(preference): prepare for unit test

---
+++ Shared/preference/androidTest/preference/OSDefaultPreferenceTest.kt
@@ -0,0 +1,16 @@
+package preference
+
+import org.junit.Before
+import org.junit.Test
+
+class OSDefaultPreferenceTest {
+    private lateinit var preference: OSDefaultPreference
+
+    @Before
+    fun createPreference() {
+    }
+
+    @Test
+    fun `test input`() {
+    }
+}
